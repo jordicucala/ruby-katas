740c2f464a1aff9a60d3fc4ff73d304cd765ef42
Marc Vinues <mrdk86@gmail.com>
2016-12-02 10:02:49 +0100

    Working with arrays

---
+++ First Kata/Marc/array.rb
@@ -0,0 +1,12 @@
+array = ['ruby', 'rails', 'test', 'gympass', 'bcn']
+
+puts "#{array.first} First array"
+puts "#{array.last} Last"
+
+puts "#{array.push('hola')} Pushing new one"
+puts "#{array.delete(array.last)} -> Delete the last one"
+puts "#{array.delete('test')} -> delete test fron #{array}"
+
+#concat
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
