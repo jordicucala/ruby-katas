6894c272f1d85d615a1e732b52393e2971b25cb0
LegaTux <josmpena91@gmail.com>
2016-12-02 10:00:24 +0100

    Analyzer: count chars without spaces

--- First Kata/Jose Manuel/analyzer.rb
+++ First Kata/Jose Manuel/analyzer.rb
@@ -1,10 +1,29 @@
 class Analyzer
-  def characters_w_space(file='../oliver.txt')
+  def chars_and_space(file='../oliver.txt')
     count = 0
-    File.foreach(file) do |line|
-      count += line.length unless line.length == 1
+    lines = file_lines(file)
+    lines.each do |line|
+      count += line.length unless line[0] == "\n"
     end
-
     count
+  end
+
+  def chars_no_space(file='../oliver.txt')
+    count = 0
+    lines = file_lines(file)
+    lines.each do |line|
+      line.each_char do |char|
+        count++ unless char == ' ' OR char == "\n"
+      end
+    end
+    count
+  end
+
+  def file_lines(file='../oliver.txt')
+    lines = []
+    File.foreach(file) do |line|
+      lines << line
+    end
+    lines
   end
 end
