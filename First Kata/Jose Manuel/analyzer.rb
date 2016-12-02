f5a739f249f8835ee8d495f53048529cbe18da0e
jordi cucala <jordicucala@hotmail.com>
2016-12-02 09:38:23 +0100

    changed code

--- First Kata/Jose Manuel/analyzer.rb
+++ First Kata/Jose Manuel/analyzer.rb
@@ -1,10 +1,11 @@
 class Analyzer
   def characters_w_space(file='../oliver.txt')
+    #Hello
     count = 0
     File.foreach(file) do |line|
       count += line.length unless line.length == 1
     end
-
-    count
+    #Test
+  
   end
 end
