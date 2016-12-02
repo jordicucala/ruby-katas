36b2f984a7bee10314423da7fce3ebdcc1e363c3
alevellop <alevellop@gmail.com>
2016-12-02 10:03:10 +0100

    array exercise

---
+++ First Kata/Alejandro/array.rb
@@ -0,0 +1,11 @@
+elements = ['ruby', 'rails', 'test', 'gympass', 'bcn']
+
+#a) Get the first element of array
+elements.first
+#b) Get the last element of array
+elements.last
+#c) Add the new element 'new'
+#d) Remove the last element
+#e) Remove the 'test' element
+#f) concat wit this array [true,true,true]
+#j) Print only pair numbers; numbers = [1,2,3,4,5,6,7,8,9,10]
