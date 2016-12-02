c169f570529412593f8216ccd71dd5a60357677c
jordi cucala <jordicucala@hotmail.com>
2016-12-02 09:57:03 +0100

    code changed

--- First Kata/Marc/analyzer.rb
+++ First Kata/Marc/analyzer.rb
@@ -1,8 +1,8 @@
 read = File.open('../oliver.txt', 'rb')
-
+#Hello Marc!!
 content = read.read
 #chars
-puts content.length
+#puts content.length
 #Excluding spaces
-puts content.split(' ').length
+#puts content.split(' ').length
 #word count
