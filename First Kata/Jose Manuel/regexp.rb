f3329fdad96f321b013e777c127d1dc7e6047eb3
LegaTux <josmpena91@gmail.com>
2016-12-02 11:13:03 +0100

    Regexp: positive_word

--- First Kata/Jose Manuel/regexp.rb
+++ First Kata/Jose Manuel/regexp.rb
@@ -1,10 +1,16 @@
 class Regex
   def valid_number?(number)
-    number.match?() || number.match?()
+    # REGEXP = /([0-9]{4}\-5{3}\-12{2})/
+    number.match(/([0-9]{4}-555-1212)/)
+    # number.match() == number || number.match() == number
   end
 
   def valid_email?(email)
     email.match(/(\w+@[a-zA-Z_]+?\.[a-zA-Z]{3})/) == email
     # email === /(\w+@[a-zA-Z_]\.(com|net|org|edu))/
+  end
+
+  def positive_word(word)
+    (word.match(/(ok|Okay|sure|yes|y)/))
   end
 end
